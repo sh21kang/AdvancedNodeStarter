e64c9109c816b4aafce79497ce2ad8c7bce070e6
sangho <sh21kan@naver.com>
2018-11-18 22:53:01 +0900

    update finish except image upload

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,7 @@ module.exports = {
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://sh21kang:michael21@ds159273.mlab.com:59273/blog_dev',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  accessKeyId :'AKIAIDG4UNUHH4JZTLAQ',
+  secretAccessKey :'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'
 };
